3c26d917fd55c404ff2e9a20801f523600654a0d
AzizbekGulomov2002 <azizgulomov1529@gmail.com>
2024-07-07 11:49:58 +0500

    --ADD Industrify-API classes

--- config/urls.py
+++ config/urls.py
@@ -8,9 +8,9 @@ from drf_yasg import openapi
 
 schema_view = get_schema_view(
     openapi.Info(
-        title="Tashkent Market API",
+        title="Industrify API",
         default_version='v1',
-        description="API documentation for Tashkent market API",
+        description="API documentation for Industrify API",
         terms_of_service="https://www.google.com/policies/terms/",
         contact=openapi.Contact(email="contact@example.com"),
         license=openapi.License(name="BSD License"),
